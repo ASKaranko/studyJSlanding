5c30d889cea962ff1f98534bc535b4bd0bf828a5
Karanko Andrei <andr.karanko@gmail.com>
2020-11-04 05:13:38 +0200

    Домашнее задание по уроку 19 Параметры документа, окна

--- lessons/script/script.js
+++ lessons/script/script.js
@@ -1,32 +1,34 @@
-window.addEventListener('DOMContentLoaded', function () {
-  'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+
+  // eslint-disable-next-line strict
+  "use strict";
 
   //Timer
   function countTimer(deadline) {
-    let timerHours = document.getElementById('timer-hours'),
+    const timerHours = document.getElementById('timer-hours'),
       timerMinutes = document.getElementById('timer-minutes'),
       timerSeconds = document.getElementById('timer-seconds');
 
     function getTimeRemaining() {
-      let dateStop = new Date(deadline).getTime(),
-      dateNow = new Date().getTime(),
-      timeRemaining = (dateStop - dateNow) / 1000,
-      seconds = Math.floor(timeRemaining % 60),
-      minutes = Math.floor((timeRemaining / 60) % 60),
-      hours = Math.floor(timeRemaining / 3600);
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 3600);
 
-      return {timeRemaining, hours, minutes, seconds};
+      return { timeRemaining, hours, minutes, seconds };
     }
 
-    function addNull (elem) {
+    function addNull(elem) {
       if (elem < 10) {
         elem = '0' + elem;
-      } 
+      }
       return elem;
     }
 
     function updateClock() {
-      let timer = getTimeRemaining();
+      const timer = getTimeRemaining();
 
       timerHours.textContent = addNull(timer.hours);
       timerMinutes.textContent = addNull(timer.minutes);
@@ -40,10 +42,80 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     }
 
-    let idInterval = setInterval(updateClock, 1000);
+    const idInterval = setInterval(updateClock, 1000);
 
   }
 
   countTimer('05 november 2020');
+
+  //Меню
+  const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+
+    closeBtn.addEventListener('click', handlerMenu);
+
+    menuItems.forEach(elem => {
+      elem.addEventListener('click', handlerMenu);
+    });
+
+  };
+
+  toggleMenu();
+
+  const animatePopUp = function(elem) {
+
+    function draw(timePassed) {
+      elem.style.left = timePassed / 25 + '%';
+    }
+
+    const start = Date.now();
+
+    const timer = setInterval(() => {
+      const timePassed = Date.now() - start;
+
+      if (timePassed > 1000) {
+        clearInterval(timer);
+        return;
+      }
+
+      draw(timePassed);
+    }, 20);
+
+  };
+
+  //Pop-up
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupContent = document.querySelector('.popup-content'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close');
+
+    popupBtn.forEach(elem => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';
+        if (window.screen.width >= 768) {
+          popupContent.style.left = 0;
+          animatePopUp(popupContent);
+        }
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+
+  };
+
+  togglePopUp();
 
 });
