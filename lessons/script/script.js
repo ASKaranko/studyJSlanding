0470fd54b7ef059f2cb7de8ff8d35f3292d3c4e5
Karanko Andrei <andr.karanko@gmail.com>
2020-11-10 02:39:06 +0200

    Домашнее задание по уроку 23 Регулярные выражение и data-атрибуты

--- lessons/script/script.js
+++ lessons/script/script.js
@@ -285,6 +285,36 @@ window.addEventListener('DOMContentLoaded', () => {
 
   };
 
+  const ourTeam = () => {
+    const command = document.getElementById('command');
+
+    command.addEventListener('mouseover', event => {
+      if (event.target.matches('.command__photo')) {
+        console.log(event.target);
+        event.target.src = event.target.dataset.img;
+      }
+    });
+
+    command.addEventListener('mouseout', event => {
+      if (event.target.matches('.command__photo')) {
+        console.log(event.target);
+        event.target.src = event.target.src.replace(/\d{1}a/, (match) => match.substring(0, 1));
+      }
+    });
+  };
+
+  const calc = () => {
+    const calcBlock = document.querySelector('.calc-block');
+
+    calcBlock.addEventListener('input', event => {
+      const target = event.target;
+      target.value = target.value.replace(/\D/g, '');
+    });
+  };
+
+  ourTeam();
+  calc();
+
   slider();
 
 });
