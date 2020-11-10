f3b11358c2dce99012b61b9a90bdadbe98b94dcf
Karanko Andrei <andr.karanko@gmail.com>
2020-11-11 00:03:31 +0200

    Домашнее задание по уроку 24 Калькулятор

--- lessons/script/script.js
+++ lessons/script/script.js
@@ -290,30 +290,70 @@ window.addEventListener('DOMContentLoaded', () => {
 
     command.addEventListener('mouseover', event => {
       if (event.target.matches('.command__photo')) {
-        console.log(event.target);
         event.target.src = event.target.dataset.img;
       }
     });
 
     command.addEventListener('mouseout', event => {
       if (event.target.matches('.command__photo')) {
-        console.log(event.target);
-        event.target.src = event.target.src.replace(/\d{1}a/, (match) => match.substring(0, 1));
+        event.target.src = event.target.src.replace(/\d{1}a/, match => match.substring(0, 1));
       }
     });
   };
 
-  const calc = () => {
-    const calcBlock = document.querySelector('.calc-block');
+  //Калькулятор
+
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = +calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value >= 5 && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+      totalValue.textContent = total;
+    };
 
     calcBlock.addEventListener('input', event => {
       const target = event.target;
-      target.value = target.value.replace(/\D/g, '');
+
+      if (target.matches('input')) {
+        target.value = target.value.replace(/\D/g, '');
+      }
+    });
+
+    calcBlock.addEventListener('change', event => {
+      const target = event.target;
+
+      if (target === calcType || target === calcSquare || target === calcCount || target === calcDay) {
+        countSum();
+      }
     });
   };
 
+
   ourTeam();
-  calc();
+  calc(100);
 
   slider();
 
